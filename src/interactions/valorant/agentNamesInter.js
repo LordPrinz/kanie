f3ada5ecb48753b17da85418e5123de528e24e3c
Paweł B <73280945+extl0l@users.noreply.github.com>
2022-08-07 18:18:01 +0200

    Updated /agents command

--- src/interactions/valorant/agentNamesInter.js
+++ src/interactions/valorant/agentNamesInter.js
@@ -1,16 +1,17 @@
-import dotenv from "dotenv"
-import fetch from "node-fetch"
+import dotenv from "dotenv";
+import fetch from "node-fetch";
 dotenv.config({});
 
-const api = process.env.API
+const api = process.env.API;
 
 export default async function agentNamesInter(interaction, region, locale) {
-    const url = `https://${region}.api.riotgames.com/val/content/v1/contents?locale=${locale}&api_key=${api}`
+    const url = `https://${region}.api.riotgames.com/val/content/v1/contents?locale=${locale}&api_key=${api}`;
     const response = await fetch(url);
 
     if (response.status !== 200) {
         interaction.reply({
-            content: "Something went wrong",
+            content: `Something went wrong (error ${response.status})`,
+            ephemeral: true
         });
         return;
     }
@@ -27,11 +28,12 @@ export default async function agentNamesInter(interaction, region, locale) {
     agents.pop(agents.length)
     let msgContent = "";
 
-    for (let index = 0; index < agents.length; index++) {
-        msgContent += `${agents[index]}\n`
-    }
+    agents.map((agent) => {
+        msgContent += `${agent}\n`
+    })
 
     interaction.reply({
         content: msgContent,
+        ephemeral: true
     });
 }
